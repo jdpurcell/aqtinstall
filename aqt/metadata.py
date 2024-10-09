192e36dc15db024441f5948eeb75be3ddea0b6f8
J.D. Purcell <jdpurcell@gmail.com>
2024-10-08 23:46:15 -0400

    Fix for win64_msvc2022_arm64_cross_compiled

--- aqt/metadata.py
+++ aqt/metadata.py
@@ -414,8 +414,10 @@ class QtRepoProperty:
 
     @staticmethod
     def default_win_msvc_desktop_arch_dir(_version: Version) -> str:
-        """_version is unused, but we expect it to matter for future releases"""
-        return "msvc2019_64"
+        if _version >= Version("6.8.0"):
+            return "msvc2022_64"
+        else:
+            return "msvc2019_64"
 
     @staticmethod
     def default_mac_desktop_arch_dir(version: Version) -> str:
