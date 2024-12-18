f3d51ec9674dcf05df26b95ebae5cdc33004b1f2
J.D. Purcell <jdpurcell@gmail.com>
2024-12-17 19:17:33 -0500

    Test build

--- aqt/installer.py
+++ aqt/installer.py
@@ -366,16 +366,14 @@ class Cli:
 
         def get_auto_desktop_archives() -> List[QtPackage]:
             def to_archives(baseurl: str) -> QtArchives:
-                # Use host_os instead of os_name for desktop Qt
-                host_os = os_name
-                if host_os == "all_os":
-                    if sys.platform.startswith("linux"):
-                        host_os = "linux"
-                    elif sys.platform == "darwin":
-                        host_os = "mac"
-                    else:
-                        host_os = "windows"
-                return QtArchives(host_os, "desktop", qt_version, cast(str, autodesk_arch), base=baseurl, timeout=timeout)
+                return QtArchives(
+                    Cli._resolve_host_os(os_name),
+                    "desktop",
+                    qt_version,
+                    cast(str, autodesk_arch),
+                    base=baseurl,
+                    timeout=timeout,
+                )
 
             if autodesk_arch is not None:
                 return cast(QtArchives, retry_on_bad_connection(to_archives, base)).archives
@@ -410,7 +408,7 @@ class Cli:
         if not nopatch:
             Updater.update(target_config, base_path, expect_desktop_archdir)
             if autodesk_arch is not None:
-                d_target_config = TargetConfig(str(_version), "desktop", autodesk_arch, os_name)
+                d_target_config = TargetConfig(str(_version), "desktop", autodesk_arch, Cli._resolve_host_os(os_name))
                 Updater.update(d_target_config, base_path, expect_desktop_archdir)
         self.logger.info("Finished installation")
         self.logger.info("Time elapsed: {time:.8f} second".format(time=time.perf_counter() - start_time))
@@ -1053,13 +1051,7 @@ class Cli:
             return None, None
 
         # For WASM installations on all_os, we need to choose a default desktop host
-        if host == "all_os":
-            if sys.platform.startswith("linux"):
-                host = "linux"
-            elif sys.platform == "darwin":
-                host = "mac"
-            else:
-                host = "windows"
+        host = Cli._resolve_host_os(host)
 
         installed_desktop_arch_dir = QtRepoProperty.find_installed_desktop_qt_dir(host, base_path, version, is_msvc=is_msvc)
         if installed_desktop_arch_dir:
@@ -1101,6 +1093,17 @@ class Cli:
                 f"          `aqt install-qt {host} desktop {version} {default_desktop_arch}`"
             )
             return expected_desktop_arch_path.name, None
+
+    @staticmethod
+    def _resolve_host_os(host: str) -> str:
+        if host != "all_os":
+            return host
+        elif sys.platform.startswith("linux"):
+            return "linux"
+        elif sys.platform == "darwin":
+            return "mac"
+        else:
+            return "windows"
 
 
 def is_64bit() -> bool:
